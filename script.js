f81223db03c76f0fc6267fc49219210efb0817c3
David Horn <horndavidg@gmail.com>
2015-05-14 09:24:23 -0700

    UPDATED JS

--- script.js
+++ script.js
@@ -44,61 +44,100 @@
 // }
 // })();
 
-
+////////////////////////////////////////////////////////////////////////////////
 
 // Able to create 2 squares but they seem to write over the other ones.
 // By console.loging the action I can see they are created 50 times but they
 // just over write one and other.  
 
 
-var tile2 = document.createElement("div");
-tile2.innerHTML = ".";
-tile2.style.backgroundColor = "black";
-tile2.style.width = "11.1%";
-tile2.style.paddingBottom = "11.1%";
-tile2.style.float = "left";
+// var tile2 = document.createElement("div");
+// tile2.innerHTML = ".";
+// tile2.style.backgroundColor = "black";
+// tile2.style.width = "11.1%";
+// tile2.style.paddingBottom = "11.1%";
+// tile2.style.float = "left";
 
 
 
-var tile = document.createElement("div");
-tile.innerHTML = ".";
-tile.style.backgroundColor = "red";
-tile.style.width = "11.1%";
-tile.style.paddingBottom = "11.1%";
-tile.style.float = "left";
+// var tile = document.createElement("div");
+// tile.innerHTML = ".";
+// tile.style.backgroundColor = "red";
+// tile.style.width = "11.1%";
+// tile.style.paddingBottom = "11.1%";
+// tile.style.float = "left";
 
 
-var body = document.querySelector("body");
+// var body = document.querySelector("body");
 
 
-// var answer = document.createElement("div");
+// // var answer = document.createElement("div");
 
-var checker = (function() {
+// var checker = (function() {
 
-	//var answer = [];
+// 	//var answer = [];
 
-	for (var i = 0; i < 50; i++) {
+// 	for (var i = 0; i < 50; i++) {
 
-	if (i % 2 === 0) {
-		//answer.push(tile2);
-console.log(body.appendChild(tile));
+// 	if (i % 2 === 0) {
+// 		//answer.push(tile2);
+// console.log(body.appendChild(tile));
 
-	} else {
-		//answer.push(tile);
-console.log(body.appendChild(tile2));
+// 	} else {
+// 		//answer.push(tile);
+// console.log(body.appendChild(tile2));
 		
+// 	}
+// }
+
+// //body.appendChild(answer);
+
+
+// //return answer;
+
+// })();
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+// UPDATED!!!!!!!
+
+
+var startingFunction = function() {
+
+
+var bod = document.querySelector("body");
+
+
+for (var i =0 ; i<9; i++)
+{
+	for (var j = 0; j < 8; j++)
+	{
+				if(j%2 === 0) {
+		
+		var tile = document.createElement("div");
+		tile.style.width = "11%";
+		tile.style.height = "100px";
+		tile.style.background = "red";
+		tile.style.float = "left";
+		document.body.appendChild(tile);
 	}
+	else
+	{
+		var tile = document.createElement("div");
+		tile.style.width = "11%";
+		tile.style.height = "100px";
+		tile.style.background = "black";
+		tile.style.float = "left";
+		document.body.appendChild(tile);
+	}
+	} 
 }
 
-//body.appendChild(answer);
+}
 
-
-//return answer;
-
-})();
-
-
-
+document.addEventListener("DOMContentLoaded", startingFunction);
 
 // // var answer = document.createElement("div");
 //  (function() {
