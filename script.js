50bf8fbbbf2456e8f85bb48c363ea95119e70cc5
David Horn <horndavidg@gmail.com>
2015-05-14 09:33:23 -0700

    adding color gradient

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your JS goes here
+
+var startingFunction = function() {
+
+
+for (var i =0 ; i<9; i++)
+{
+	for (var j = 0; j < 8; j++)
+	{
+
+		
+
+			var rando1 = Math.floor(Math.random() * 255);
+			var rando2 = Math.floor(Math.random() * 255);
+			var rando3 = Math.floor(Math.random() * 255);
+
+		var tile = document.createElement("div");
+		tile.style.width = "11%";
+		tile.style.height = "100px";
+		tile.style.background = "rgb("+ rando1 + "," + rando2 + "," + rando3 + ")";
+		tile.style.float = "left";
+		document.body.appendChild(tile);
+		
+	
+	} 
+}
+
+}
+
+document.addEventListener("DOMContentLoaded", startingFunction);
